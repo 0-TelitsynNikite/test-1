187d1aeb8653edb9cd2e2d1ac68b2a1f44bd5d94
Телицын Никита <77964273+0-TelitsynNikite@users.noreply.github.com>
2022-06-29 18:20:26 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,42 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   map: Map<number, null> = new Map()
-  data: string[][] = [["36", "не приватизирован", "4 собственника", "Парковая"],
+  data: string[][] = [
+    ["36", "не приватизирован", "4 собственника", "Парковая"],
     ["45", "приватизирован", "3 собственника", "Луговая"],
     ["56", "не приватизирован", "1 собственник", "Луговая"],
     ["56", "приватизирован", "5 собственников", "Ленина"],
     ["25", "не приватизирован", "2 собственника", "Ленина"],
-    ["40", "не приватизирован", "2 собственника", "Ленина"]]
+    ["40", "не приватизирован", "2 собственника", "Ленина"]
+]
 
-  changeMap(): void {
-    for (let i = 0; i < 100; i++) {
-      this.map.set(i, null)
-
-      if (this.map.has(i) && i % 5 === 0 && i % 3 === 0 && i !== 0) {
+  fizzBuzz(value = this.map.keys()) {
+    for (let key of value) {
+      if (key % 5 === 0 && key % 3 === 0) {
         console.log('I can believe in that!')
-      } else if (this.map.has(i) && i % 5 === 0) {
+      } else if (key % 5 === 0) {
         console.log('Oops, here is 5')
-      } else if (this.map.has(i) && i % 3 === 0) {
+      } else if (key % 3 === 0) {
         console.log('Devided by tree')
       }
     }
   }
 
+  addValueToMap(): void {
+    for (let i = 0; i < 100; i++) {
+      this.map.set(i, null)
+    }
+
+    this.fizzBuzz()
+  }
+
   getAreaOfAHouse(arr: string[][] = this.data): void {
     for (let item of arr) {
-      if (item.some(a => a === '40') && item.some(a => a === 'Ленина')) {
+      let [numberOfHouse, statusOfPrivatisation, numberOfOwners, streetName] = item
+
+      if (numberOfHouse === '40' && streetName === 'Ленина') {
         console.log(true)
-      } else if (item.some(a => a === '56' && item.some(a => a !== 'Ленина'))) {
+      } else if (numberOfHouse === '56' && streetName !== 'Ленина') {
         console.log(true)
       } else {
         console.log(false)
